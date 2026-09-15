e2c4ebb2364ce13fd2164a1d59eac65633a543f1
Amilton Oliveira <amilton.o.sousa@gmail.com>
2026-09-14 23:31:02 -0300

    chore: remover pasta target

--- target/debug/banco-memoria-ep1.d
+++
@@ -1,1 +0,0 @@
-C:\projetos\ep1-banco-de-dados\target\debug\banco-memoria-ep1.exe: C:\projetos\ep1-banco-de-dados\src\main.rs C:\projetos\ep1-banco-de-dados\src\storage.rs
--- target/debug/deps/banco_memoria_ep1.d
+++
@@ -1,6 +0,0 @@
-C:\projetos\ep1-banco-de-dados\target\debug\deps\banco_memoria_ep1.d: src\main.rs src\storage.rs
-
-C:\projetos\ep1-banco-de-dados\target\debug\deps\banco_memoria_ep1.exe: src\main.rs src\storage.rs
-
-src\main.rs:
-src\storage.rs:
